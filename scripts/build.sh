11d5d7eba9b7f46c5ae46b724f74591a6bcb3995
Kris Saxton <kris@automationlogic.com>
2012-10-04 00:19:18 +0100

    added LDAP module and ext_pillar backend

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Used by github actions to build wheels in a CI/CD container.
-apt-get update;
-apt-get install -y gcc python3 \
-   python3-pip python3-venv build-essential patchelf m4 texinfo groff-base
-cd /src
-python3 -m venv venv
-venv/bin/pip install build wheel setuptools
-venv/bin/python3 -m build
--- tests/test_saltext_ldap.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2024 Broadcom Corporation
-# SPDX-License-Identifier: Apache-2.0
-#
-def test_noop():
-    return
