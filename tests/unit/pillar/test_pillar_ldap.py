c1fb37def06750eb6476c66baf29056b664a2d10
Caleb Beard <53276404+MKLeb@users.noreply.github.com>
2022-09-25 20:28:42 -0400

    migrate pillar unit tests to pytest (#62745)

--- tests/unit/pillar/test_pillar_ldap.py
+++ tests/unit/pillar/test_pillar_ldap.py
@@ -1,18 +1,20 @@
+import salt.pillar.pillar_ldap as pillar_ldap
 import salt.utils.stringutils
-from salt.pillar.pillar_ldap import _config
-from tests.support.unit import TestCase
 
 
-class LdapPillarTestCase(TestCase):
-    def test__config_returns_str(self):
-        conf = {"foo": "bar"}
-        assert _config("foo", conf) == salt.utils.stringutils.to_str("bar")
+def test__config_returns_str():
+    conf = {"foo": "bar"}
+    assert pillar_ldap._config("foo", conf) == salt.utils.stringutils.to_str("bar")
 
-    def test__conf_defaults_to_none(self):
-        conf = {"foo": "bar"}
-        assert _config("bang", conf) is None
 
-    def test__conf_returns_str_from_unicode_default(self):
-        conf = {"foo": "bar"}
-        default = salt.utils.stringutils.to_unicode("bam")
-        assert _config("bang", conf, default) == salt.utils.stringutils.to_str("bam")
+def test__conf_defaults_to_none():
+    conf = {"foo": "bar"}
+    assert pillar_ldap._config("bang", conf) is None
+
+
+def test__conf_returns_str_from_unicode_default():
+    conf = {"foo": "bar"}
+    default = salt.utils.stringutils.to_unicode("bam")
+    assert pillar_ldap._config("bang", conf, default) == salt.utils.stringutils.to_str(
+        "bam"
+    )
