1b8f8d78a33823141530e599209d51e40dfd0364
Frode Gundersen <fgundersen@vmware.com>
2022-12-06 20:30:33 +0000

    migrate test_ldapmod to pytest

--- tests/unit/modules/test_ldapmod.py
+++ tests/unit/modules/test_ldapmod.py
@@ -1,63 +1,58 @@
 """
     :codeauthor: Jayesh Kariya <jayeshk@saltstack.com>
+
+    Test cases for salt.modules.ldapmod
 """
 
 
 import time
 
+import pytest
+
 import salt.modules.ldapmod as ldapmod
-from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.mock import MagicMock, patch
-from tests.support.unit import TestCase
 
 
-class LdapmodTestCase(TestCase, LoaderModuleMockMixin):
+@pytest.fixture
+def configure_loader_modules():
+    return {ldapmod: {}}
+
+
+# 'search' function tests: 1
+
+
+def test_search():
     """
-    Test cases for salt.modules.ldapmod
+    Test if it run an arbitrary LDAP query and return the results.
     """
 
-    def setup_loader_modules(self):
-        return {ldapmod: {}}
-
-    # 'search' function tests: 1
-
-    def test_search(self):
+    class MockConnect:
         """
-        Test if it run an arbitrary LDAP query and return the results.
+        Mocking _connect method
         """
 
-        class MockConnect:
+        def __init__(self):
+            self.bdn = None
+            self.scope = None
+            self._filter = None
+            self.attrs = None
+
+        def search_s(self, bdn, scope, _filter, attrs):
             """
-            Mocking _connect method
+            Mock function for search_s
             """
+            self.bdn = bdn
+            self.scope = scope
+            self._filter = _filter
+            self.attrs = attrs
+            return "SALT"
 
-            def __init__(self):
-                self.bdn = None
-                self.scope = None
-                self._filter = None
-                self.attrs = None
-
-            def search_s(self, bdn, scope, _filter, attrs):
-                """
-                Mock function for search_s
-                """
-                self.bdn = bdn
-                self.scope = scope
-                self._filter = _filter
-                self.attrs = attrs
-                return "SALT"
-
-        mock = MagicMock(return_value=True)
-        with patch.dict(ldapmod.__salt__, {"config.option": mock}):
-            with patch.object(
-                ldapmod, "_connect", MagicMock(return_value=MockConnect())
-            ):
-                with patch.object(time, "time", MagicMock(return_value=8e-04)):
-                    self.assertDictEqual(
-                        ldapmod.search(filter="myhost"),
-                        {
-                            "count": 4,
-                            "results": "SALT",
-                            "time": {"raw": "0.0", "human": "0.0ms"},
-                        },
-                    )
+    mock = MagicMock(return_value=True)
+    with patch.dict(ldapmod.__salt__, {"config.option": mock}):
+        with patch.object(ldapmod, "_connect", MagicMock(return_value=MockConnect())):
+            with patch.object(time, "time", MagicMock(return_value=8e-04)):
+                assert ldapmod.search(filter="myhost") == {
+                    "count": 4,
+                    "results": "SALT",
+                    "time": {"raw": "0.0", "human": "0.0ms"},
+                }
