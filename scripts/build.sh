bdabbebd24fbf64b987ffccb8d2dfa08a5988d0e
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-09-09 02:49:35 -0700

    Update package action artifacts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Used by github actions to build wheels in a CI/CD container.
 apt-get update;
 apt-get install -y gcc python3 \
    python3-pip python3-venv build-essential patchelf m4 texinfo groff-base
