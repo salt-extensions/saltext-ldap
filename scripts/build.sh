50cc7f5a70c557ffbc15bbfb24d302fdd03a3c28
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2024-09-09 00:02:52 -0700

    Try to build

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+apt-get update;
+apt-get install -y gcc python3 \
+   python3-pip python3-venv build-essential patchelf m4 texinfo
+cd /src
+python3 -m venv venv
+venv/bin/pip install build wheel setuptools
+venv/bin/python3 -m build
